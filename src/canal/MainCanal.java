83b6149d2f717e0f36ae05512585f98c8163ff07
rbaluta17dw <Robert@DESKTOP-2J3L58J>
2018-01-18 21:53:14 +0100

    siguiente

--- src/canal/MainCanal.java
+++ src/canal/MainCanal.java
@@ -3,14 +3,22 @@ package canal;
 public class MainCanal {
 
 	public static void main(String[] args) {
-		final int CREAR = 1;
-		final int LEER = 2;
-		final int ACTUALIZAR = 3;
-		final int BORRAR = 4;
+		final int CREARC = 1;
+		final int SELEC = 2;
+		final int LEERC = 3;
+		final int ACTUALIZARC = 4;
+		final int BORRARC = 5;
 		final int SALIR = 0;
 		int opcion;
 		
-		
+		do {
+			System.out.println(CREARC + "- Crear canal de television");
+			System.out.println(SELEC + "- Seleccionar canal de television");
+			System.out.println(LEERC + "- Mostrar lista de canales de television");
+			System.out.println(ACTUALIZARC + "- Cambiar si el canal de television es privado o publico");
+			System.out.println(BORRARC + "- Borrar un canal de television");
+			System.out.println();
+		}while(opcion != SALIR);
 	}
 
 }
